7d519d61f12e02f15cc4a05e301600b7e4fe52e9
KITE <ixkaito@gmail.com>
2015-05-02 00:18:37 +0900

    Remove post nav background

--- functions.php
+++ functions.php
@@ -271,50 +271,8 @@ function tui_post_link( $output, $format, $link, $post, $adjacent ) {
 	$output = preg_replace( '/(<div class="nav-.*?">)/', "$1$meta_nav", $output );
 	return $output;
 }
-add_filter( 'previous_post_link', 'tui_post_link', 9, 5 );
-add_filter( 'next_post_link', 'tui_post_link', 9, 5 );
-
-/**
- * Add featured image as background image to post navigation elements.
- *
- * @since TUI 1.0
- *
- * @see wp_add_inline_style()
- */
-function tui_post_nav_background() {
-	if ( ! is_single() ) {
-		return;
-	}
-
-	$previous = ( is_attachment() ) ? get_post( get_post()->post_parent ) : get_adjacent_post( false, '', true );
-	$next     = get_adjacent_post( false, '', false );
-	$css      = '';
-
-	if ( is_attachment() && 'attachment' == $previous->post_type ) {
-		return;
-	}
-
-	if ( $previous &&  has_post_thumbnail( $previous->ID ) ) {
-		$prevthumb = wp_get_attachment_image_src( get_post_thumbnail_id( $previous->ID ), 'post-thumbnail' );
-		$css .= '
-			.post-navigation .nav-previous { background-image: url(' . esc_url( $prevthumb[0] ) . '); }
-			.post-navigation .nav-previous .post-title, .post-navigation .nav-previous a:hover .post-title, .post-navigation .nav-previous .meta-nav { color: #fff; }
-			.post-navigation .nav-previous a:before { background-color: rgba(0, 0, 0, 0.4); }
-		';
-	}
-
-	if ( $next && has_post_thumbnail( $next->ID ) ) {
-		$nextthumb = wp_get_attachment_image_src( get_post_thumbnail_id( $next->ID ), 'post-thumbnail' );
-		$css .= '
-			.post-navigation .nav-next { background-image: url(' . esc_url( $nextthumb[0] ) . '); border-top: 0; }
-			.post-navigation .nav-next .post-title, .post-navigation .nav-next a:hover .post-title, .post-navigation .nav-next .meta-nav { color: #fff; }
-			.post-navigation .nav-next a:before { background-color: rgba(0, 0, 0, 0.4); }
-		';
-	}
-
-	wp_add_inline_style( 'tui-style', $css );
-}
-add_action( 'wp_enqueue_scripts', 'tui_post_nav_background' );
+add_filter( 'previous_post_link', 'tui_post_link', 10, 5 );
+add_filter( 'next_post_link', 'tui_post_link', 10, 5 );
 
 /**
  * Display descriptions in main navigation.
