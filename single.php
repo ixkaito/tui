485685b36b040085fe319d9323ad31a3df000c68
KITE <ixkaito@gmail.com>
2015-05-01 16:17:06 +0900

    Update post-navigation

--- single.php
+++ single.php
@@ -30,11 +30,9 @@ get_header(); ?>
 
 			// Previous/next post navigation.
 			the_post_navigation( array(
-				'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next', 'tui' ) . '</span> ' .
-					'<span class="screen-reader-text">' . __( 'Next post:', 'tui' ) . '</span> ' .
+				'next_text' => '<span class="screen-reader-text">' . __( 'Next post:', 'tui' ) . '</span> ' .
 					'<span class="post-title">%title</span>',
-				'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Previous', 'tui' ) . '</span> ' .
-					'<span class="screen-reader-text">' . __( 'Previous post:', 'tui' ) . '</span> ' .
+				'prev_text' => '<span class="screen-reader-text">' . __( 'Previous post:', 'tui' ) . '</span> ' .
 					'<span class="post-title">%title</span>',
 			) );
 
